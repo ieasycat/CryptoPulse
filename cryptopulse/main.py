dedc2e8d885aa6f2971c612cbf30ee269e1be3b8
Anton <ieasycat@hotmail.com>
2024-09-18 20:12:59 +0200

    Add orjson

--- cryptopulse/main.py
+++ cryptopulse/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 import uvicorn
 
@@ -17,7 +18,10 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 
-main_app = FastAPI(lifespan=lifespan)
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
 main_app.include_router(
     cryptopulse_router, prefix=settings.api.prefix, tags=settings.api.tags
 )
