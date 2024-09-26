ab988ec0b56defa13c05c5311b59f8eaae29457b
Anton <ieasycat@hotmail.com>
2024-09-26 11:08:49 +0200

    Update views.py

--- cryptopulse/api/views.py
+++ cryptopulse/api/views.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
 from core.schemas.schemas import CryptoPulseSchema
-from api.controller import CryptoPulseController
+from core.controller import CryptoPulseController
 
 router = APIRouter()
 
