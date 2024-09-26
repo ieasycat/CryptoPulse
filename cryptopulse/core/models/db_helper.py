041dff5cdda0df4d055201757da61937ac3239c5
Anton <ieasycat@hotmail.com>
2024-09-26 11:05:34 +0200

    Update db_helper.py

--- cryptopulse/core/models/db_helper.py
+++ cryptopulse/core/models/db_helper.py
@@ -32,7 +32,7 @@ class DatabaseHelper:
         )
 
     async def dispose(self) -> None:
-        await self.dispose()
+        await self.engine.dispose()
 
     async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
